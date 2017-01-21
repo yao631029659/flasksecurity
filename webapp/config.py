c85da699dd851f197f72eb3cea5473e22fbfc139
yoyo3 <yao631029659@126.com>
2017-01-21 11:42:03 +0800

    登陆自定义实现

--- webapp/config.py
+++ webapp/config.py
@@ -3,3 +3,5 @@ class Config(object):
     SECRET_KEY = '6LfOUhAUAAAAAHiszVGQsv9VO2NCBRsHbEwsvOxr'
     # SQLALCHEMY_DATABASE_URI = 'sqlite:///database.db'
     SQLALCHEMY_DATABASE_URI = 'sqlite://'
+    # 允许自定义视图模板
+    SECURITY_REGISTERABLE = True
